310b00be74817f3d3975a8629a9c29366ecd4667
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2018-10-07 15:39:53 +0100

    Updated data installation scripts.

--- src/main/scripts/install_kilmarnock_data.sh
+++ src/main/scripts/install_kilmarnock_data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-scp -rq secure@manifesto.cs.st-andrews.ac.uk:/home/secure/maven_repo/uk/ac/standrews/cs/data-kilmarnock/1.0-SNAPSHOT/data-kilmarnock-*-jar-with-dependencies.jar data-kilmarnock-jar-with-dependencies.jar
-mvn -q install:install-file -Dfile=data-kilmarnock-jar-with-dependencies.jar -DgroupId=uk.ac.standrews.cs -DartifactId=data-kilmarnock -Dversion=1.0-SNAPSHOT -Dpackaging=jar
-rm data-kilmarnock-jar-with-dependencies.jar
+scp -q secure@manifesto.cs.st-andrews.ac.uk:/home/secure/maven_repo/uk/ac/standrews/cs/data-kilmarnock/1.0/data-kilmarnock-1.0-jar-with-dependencies.jar .
+mvn -q install:install-file -Dfile=data-kilmarnock-1.0-jar-with-dependencies.jar -DgroupId=uk.ac.standrews.cs -DartifactId=data-kilmarnock -Dversion=1.0 -Dpackaging=jar
+rm data-kilmarnock-1.0-jar-with-dependencies.jar
---
+++ src/main/scripts/manifesto/README.md
@@ -0,0 +1,3 @@
+# Data Installation Scripts for manifesto
+
+These are different for manifesto, since the maven repository is local.
