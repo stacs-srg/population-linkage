71073c034d2d259b363fef1d1215b38b1346c472
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2019-03-04 11:31:44 +0000

    Added new shell script.

---
+++ src/main/scripts/experiments/all_pairs_umea_bride_birth.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [[ -n "$1" ]];
+then
+    export MAVEN_OPTS="-Xmx"$1"G"
+    echo Setting heap size: $1GB
+fi
+
+
+mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.population_linkage.groundTruth.AllPairsBrideMarriageBirthUmeaIdentityLinkage" -e
