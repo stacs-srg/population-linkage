121c79ce13db3805ea83f36021317ea86e9b65e7
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2019-03-12 11:42:51 +0000

    Changed run_all script.

--- src/main/scripts/experiments/run_all.sh
+++ src/main/scripts/experiments/run_all.sh
@@ -6,7 +6,6 @@ echo please run this from project root = population-linkage
 src/main/scripts/experiments/umea_birth_father.sh 100
 src/main/scripts/experiments/umea_birth_mother.sh 100
 src/main/scripts/experiments/umea_birth_sibling.sh 100
-src/main/scripts/experiments/umea_bit_blaster_sibling_bundling.sh 100
 src/main/scripts/experiments/umea_bride_birth.sh 100
 src/main/scripts/experiments/umea_bride_bride_sibling.sh 100
 src/main/scripts/experiments/umea_bride_groom_sibling.sh 100
