e936a5b5053406eece56e2d3905753db27c90856
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2019-04-04 09:07:48 +0100

    Added new experiment to do temporal windowing - only for BirthFather initially.

---
+++ src/main/java/uk/ac/standrews/cs/population_linkage/groundTruth/UmeaBirthFatherAgeFiltered.java
@@ -0,0 +1,84 @@
+package uk.ac.standrews.cs.population_linkage.groundTruth;
+
+import uk.ac.standrews.cs.population_linkage.data.Utilities;
+import uk.ac.standrews.cs.population_linkage.linkage.ApplicationProperties;
+import uk.ac.standrews.cs.population_linkage.metrics.Sigma2AgeFiltered;
+import uk.ac.standrews.cs.population_records.RecordRepository;
+import uk.ac.standrews.cs.population_records.record_types.Birth;
+import uk.ac.standrews.cs.storr.impl.LXP;
+import uk.ac.standrews.cs.utilities.metrics.coreConcepts.NamedMetric;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* This class performs linkage analysis on data from birth certificates.
+ * It compares the baby's names on a birth certificate with the father's names on another birth certificate.
+ * It attempts to find the a person in the roles of baby and father on two different certificates, and thus is identity linkage
+ * The fields used for comparison are listed in getComparisonFields() and getComparisonFields2().
+ * The ground truth is listed in isTrueLink.
+ **/
+
+public class UmeaBirthFatherAgeFiltered extends AsymmetricSingleSourceLinkageAnalysis {
+
+    public UmeaBirthFatherAgeFiltered(Path store_path, String repo_name, String linkage_results_filename, final String distance_results_filename, int number_of_records_to_be_checked, int number_of_runs) throws IOException {
+        super(store_path,repo_name,linkage_results_filename, distance_results_filename,number_of_records_to_be_checked,number_of_runs);
+    }
+
+    @Override
+    public Iterable<LXP> getSourceRecords(RecordRepository record_repository) {
+        return Utilities.getBirthRecords( record_repository );
+    }
+
+    @Override
+    protected LinkStatus isTrueLink(LXP record1, LXP record2) {
+
+        final String record_id1 = record1.getString(Birth.CHILD_IDENTITY);
+        final String record_id2 = record2.getString(Birth.FATHER_IDENTITY);
+
+        if (record_id1.isEmpty() || record_id2.isEmpty()) return LinkStatus.UNKNOWN;
+
+        return record_id1.equals(record_id2) ? LinkStatus.TRUE_LINK : LinkStatus.NOT_TRUE_LINK;
+    }
+
+    @Override
+    protected String getSourceType() {
+        return "births";
+    }
+
+    @Override
+    public List<Integer> getComparisonFields() {
+        return Arrays.asList(
+                Birth.FORENAME,
+                Birth.SURNAME );
+    }
+
+    @Override
+    public List<Integer> getComparisonFields2() {
+        return Arrays.asList(
+                Birth.FATHER_FORENAME,
+                Birth.FATHER_SURNAME );
+    }
+
+    @Override
+    protected List<NamedMetric<LXP>> getCombinedMetrics() {
+
+        final List<NamedMetric<LXP>> result = new ArrayList<>();
+
+        for (final NamedMetric<String> base_metric : Utilities.BASE_METRICS) {
+            result.add(new Sigma2AgeFiltered(base_metric, getComparisonFields(), getComparisonFields2(), Birth.BIRTH_YEAR, Birth.BIRTH_YEAR ));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Path store_path = ApplicationProperties.getStorePath();
+        String repo_name = "umea";
+        int NUMBER_OF_RUNS = 1;
+
+        new UmeaBirthFatherAgeFiltered(store_path, repo_name, getLinkageResultsFilename(), getDistanceResultsFilename(), DEFAULT_NUMBER_OF_RECORDS_TO_BE_CHECKED,NUMBER_OF_RUNS).run();
+    }
+}
---
+++ src/main/java/uk/ac/standrews/cs/population_linkage/metrics/Sigma2AgeFiltered.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017 Systems Research Group, University of St Andrews:
+ * <https://github.com/stacs-srg>
+ *
+ * This file is part of the module linkage-java.
+ *
+ * linkage-java is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * linkage-java is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with linkage-java. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.population_linkage.metrics;
+
+
+import uk.ac.standrews.cs.storr.impl.LXP;
+import uk.ac.standrews.cs.utilities.metrics.coreConcepts.NamedMetric;
+
+import java.util.List;
+
+/**
+ * Sigma function for combining metrics  - compares different field lists over data
+ * Created by al on 13/12/18
+ */
+public class Sigma2AgeFiltered extends Sigma2 implements NamedMetric<LXP>  {
+
+    protected Integer birthyear_field1;
+    protected Integer birthyear_field2;
+
+    final static int sexual_maturity_age = 14; // father must be older than child by this amount
+    final static int max_father_age = 80;
+
+    public Sigma2AgeFiltered(NamedMetric<String> baseDistance, List<Integer> fields1, List<Integer> fields2, Integer birthyear_field1, Integer birthyear_field2   ) {
+
+        super( baseDistance,fields1,fields2);
+        this.birthyear_field1 = birthyear_field1;
+        this.birthyear_field2 = birthyear_field2;
+    }
+
+
+    @Override
+    // gives distance of 1 for records in which the dob for a is earlier by sexual_maturity_age than dob for b
+    // and for records for which b's dob is more than 80 after a's dob
+    // years are enough
+    public double distance(LXP father_birth_cert, LXP child_birth_cert) {
+
+        try {
+            String father_yob_as_string = father_birth_cert.getString(birthyear_field1);
+            String child_yob_as_string = child_birth_cert.getString(birthyear_field2);
+
+            int father_yob = Integer.parseInt(father_yob_as_string);
+            int child_yob = Integer.parseInt(child_yob_as_string);
+
+            if ( child_yob > father_yob + max_father_age || father_yob + sexual_maturity_age < child_yob ) {
+                return 1;
+            }
+        } catch( NumberFormatException e ) {
+            // eat exception and just do distance calc normally
+        }
+        return super.distance(father_birth_cert,child_birth_cert);
+    }
+
+    @Override
+    public String getMetricName() {
+        return "Sigma2AgeFiltered" + "-" + baseDistance.getMetricName() + "-" + hyphenConcat( fieldList1 ) + "--" + hyphenConcat( fieldList2 );
+    }
+
+    private static String hyphenConcat(List<Integer> fieldList) {
+        StringBuilder sb = new StringBuilder();
+        for( int i = 0; i < fieldList.size() - 1; i++ ) {
+            sb.append( Integer.toString( fieldList.get(i) ) );
+            sb.append( "-" );
+        }
+        sb.append( Integer.toString( fieldList.get( fieldList.size() - 1 ) ) );
+        return sb.toString();
+    }
+
+}
+
