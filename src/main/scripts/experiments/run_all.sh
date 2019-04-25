aafdf8b16db06140b5fdf8b36bddb4fb3426390a
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2019-04-25 09:49:59 +0100

    Fixed shell scripts for running on Manifesto.

--- src/main/scripts/experiments/run_all.sh
+++ src/main/scripts/experiments/run_all.sh
@@ -5,19 +5,25 @@ echo please run this from project root = population-linkage
 
 src/main/scripts/experiments/umea_birth_father.sh 100
 src/main/scripts/experiments/umea_birth_mother.sh 100
-src/main/scripts/experiments/umea_birth_sibling.sh 100
+src/main/scripts/experiments/umea_death_birth.sh 100
 
-src/main/scripts/experiments/umea_bride_birth.sh 100
-src/main/scripts/experiments/umea_bride_groom_sibling.sh 100
-src/main/scripts/experiments/umea_bride_bride_sibling.sh 100
-src/main/scripts/experiments/umea_bride_parents.sh 100
+src/main/scripts/experiments/umea_birth_sibling.sh 100
+src/main/scripts/experiments/umea_death_sibling.sh 100
 
 src/main/scripts/experiments/umea_groom_birth.sh 100
-src/main/scripts/experiments/umea_groom_bride_sibling.sh 100
-src/main/scripts/experiments/umea_groom_groom_sibling.sh 100
-src/main/scripts/experiments/umea_groom_parents.sh 100
+src/main/scripts/experiments/umea_bride_birth.sh 100
 
-src/main/scripts/experiments/umea_death_birth.sh 100
-src/main/scripts/experiments/umea_death_sibling.sh 100
+src/main/scripts/experiments/umea_groom_groom_sibling.sh 100
+src/main/scripts/experiments/umea_bride_bride_sibling.sh 100
+src/main/scripts/experiments/umea_bride_groom_sibling.sh 100
+src/main/scripts/experiments/umea_groom_bride_sibling.sh 100
+
+src/main/scripts/experiments/umea_birth_father_filtered.sh 100
+
+
+
+
+
+
 
 
