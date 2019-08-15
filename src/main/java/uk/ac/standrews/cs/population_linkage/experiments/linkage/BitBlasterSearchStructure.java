5e5e592aa9af9b553a983df0bbe4cd6e24d55a5f
tomsdalton <tsd4@st-andrews.ac.uk>
2019-08-15 12:35:47 +0100

    Further bitblaster tweaks

--- src/main/java/uk/ac/standrews/cs/population_linkage/experiments/linkage/BitBlasterSearchStructure.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/experiments/linkage/BitBlasterSearchStructure.java
@@ -2,6 +2,7 @@ package uk.ac.standrews.cs.population_linkage.experiments.linkage;
 
 import uk.ac.standrews.cs.utilities.metrics.coreConcepts.DataDistance;
 import uk.ac.standrews.cs.utilities.metrics.coreConcepts.Metric;
+import uk.al_richard.metricbitblaster.production.ParallelBitBlaster;
 import uk.al_richard.metricbitblaster.production.ParallelBitBlaster2;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ public class BitBlasterSearchStructure<T> implements SearchStructure<T> {
 
     private static final int DEFAULT_NUMBER_OF_REFERENCE_POINTS = 20;
     private static final long SEED = 34258723425L;
-    private ParallelBitBlaster2<T> bit_blaster;
+    private ParallelBitBlaster<T> bit_blaster;
 
     public BitBlasterSearchStructure(Metric<T> distance_metric, Iterable<T> data) {
 
@@ -33,7 +34,8 @@ public class BitBlasterSearchStructure<T> implements SearchStructure<T> {
     private void init(final Metric<T> distance_metric, final List<T> reference_points, final List<T> data) {
 
         try {
-            bit_blaster = new ParallelBitBlaster2<>(distance_metric::distance, reference_points, data, 2, false, true);
+//            bit_blaster = new ParallelBitBlaster2<>(distance_metric::distance, reference_points, data, 2, false, true);
+            bit_blaster = new ParallelBitBlaster<>(distance_metric::distance, reference_points, data, 2, true);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
