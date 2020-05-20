18fd94607840fefca6ce403984f7da2092882fad
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2020-05-20 09:03:07 +0100

    Updated licence notices.

--- src/main/java/uk/ac/standrews/cs/population_linkage/groundTruth/UmeaDeathSiblingExamineRecordsAlExperiment.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/groundTruth/UmeaDeathSiblingExamineRecordsAlExperiment.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2020 Systems Research Group, University of St Andrews:
+ * <https://github.com/stacs-srg>
+ */
 package uk.ac.standrews.cs.population_linkage.groundTruth;
 
 import uk.ac.standrews.cs.population_linkage.supportClasses.Utilities;
