9a1b0d0ed62227d293f9fdf0581a865b09f51f66
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2020-06-01 15:19:42 +0100

    Added install_umea_jar

---
+++ src/main/scripts/install/install_umea_jar.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 Systems Research Group, University of St Andrews:
+# <https://github.com/stacs-srg>
+#
+
+
+mvn -q install:install-file -Dfile=$1 -DgroupId=uk.ac.standrews.cs -DartifactId=UMEA-DATA -Dversion=1.0-SNAPSHOT -Dpackaging=jar
