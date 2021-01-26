3daa405eb739d4e4830f2eb0b08fb4681c5d9215
tdalton <tdalton@adobe.com>
2021-01-26 22:38:08 +0000

    removed errant license headers

--- src/main/java/uk/ac/standrews/cs/population_linkage/helpers/jobq/job/JobCore.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/helpers/jobq/job/JobCore.java
@@ -1,19 +1,3 @@
-/*
- * ************************************************************************
- *
- * Copyright 2021 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- * ************************************************************************
- */
 package uk.ac.standrews.cs.population_linkage.helpers.jobq.job;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
