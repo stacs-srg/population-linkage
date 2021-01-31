601497c67f432d51488e9e682bbb6c5a1db1d906
tdalton <tdalton@adobe.com>
2021-01-31 19:58:52 +0000

    Script fix

--- src/main/scripts/experiments/synthetic/runClusterLinkageJobQInstance.sh
+++ src/main/scripts/experiments/synthetic/runClusterLinkageJobQInstance.sh
@@ -4,4 +4,4 @@
 #
 
 cd population-linkage
-sh src/main/scripts/experiments/synthetic/run_linkage_job_q_instance.sh "12 cluster-runs/jobQ/job-q.csv cluster-runs/record-counts.csv cluster-runs/status.txt cluster-runs/gt-link-counts.csv" > runs/job-run-`hostname`.txt
+sh src/main/scripts/experiments/synthetic/run_linkage_job_q_instance.sh 12 "12 cluster-runs/jobQ/job-q.csv cluster-runs/record-counts.csv cluster-runs/status.txt cluster-runs/gt-link-counts.csv" > runs/job-run-`hostname`.txt
