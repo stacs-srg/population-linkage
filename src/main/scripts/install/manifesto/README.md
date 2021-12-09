82a120ba5db4cf87a86a2931c75d82ad9289b6f9
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2021-12-09 16:06:35 +0000

    Removed now redundant manual mvn installation scripts.

--- src/main/scripts/install/manifesto/README.md
+++
@@ -1,3 +0,0 @@
-# Data Installation Scripts for manifesto
-
-These are different for manifesto, since the maven repository is local.
--- src/main/scripts/install/setup_umea_in_storr.sh
+++ src/main/scripts/install/setup_umea_in_storr.sh
@@ -6,13 +6,10 @@
 
 export MAVEN_OPTS="-Xmx16G"
 
-echo "This script assumes script install_umea_data has already been run to install into the local maven repository"
+echo "This script assumes that the script put_umea_into_storr.sh has already been run from the data-umea project"
 
-echo "1. Loading event records into Storr"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.data.umea.store.ImportUmeaRecordsToStore" -e
-
-echo "2. Indexing records with STANDARDISED_ID in Neo4J"
+echo "Indexing records with STANDARDISED_ID in Neo4J"
 mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.population_linkage.graph.IndexOnStandardisedId" -e
 
-echo "3. Establishing GT linkage in Neo4J"
+echo "Establishing GT linkage in Neo4J"
 mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.population_linkage.groundTruth.groundTruthNeoLinks.CreateGTLinks" -e
