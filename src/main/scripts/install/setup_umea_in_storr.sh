8800b406ed92ce0ac95c74bafe60c8425f0082da
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2021-12-14 18:37:13 +0000

    Removed logic to create and delete Neo4j indices (moved to data-umea).

--- src/main/scripts/install/setup_umea_in_storr.sh
+++ src/main/scripts/install/setup_umea_in_storr.sh
@@ -6,10 +6,7 @@
 
 export MAVEN_OPTS="-Xmx16G"
 
-echo "This script assumes that the script put_umea_into_storr.sh has already been run from the data-umea project"
+echo "This script assumes that the script setup_umea_in_storr.sh has already been run from the data-umea project"
 
-echo "Indexing records with STANDARDISED_ID in Neo4J"
-mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.population_linkage.graph.IndexOnStandardisedId" -e
-
-echo "Establishing GT linkage in Neo4J"
+echo "Establishing ground truth linkage in Neo4J"
 mvn exec:java -q -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="uk.ac.standrews.cs.population_linkage.groundTruth.groundTruthNeoLinks.CreateGTLinks" -e
