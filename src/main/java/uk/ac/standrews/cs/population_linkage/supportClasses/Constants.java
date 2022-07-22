82ac6a3917e83231e5044c7803cf85d49e6275ba
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2022-07-22 15:37:24 +0100

    Debugging non-termination on Linux.

--- src/main/java/uk/ac/standrews/cs/population_linkage/supportClasses/Constants.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/supportClasses/Constants.java
@@ -45,7 +45,8 @@ public class Constants {
     public static final List<StringMeasure> PHONETIC_COMPARATORS = List.of(
             METAPHONE, NYSIIS);
 
-    public static final List<StringMeasure> BASE_MEASURES = concatenate(TRUE_METRICS, NON_METRIC_MEASURES, PHONETIC_COMPARATORS);
+    public static final List<StringMeasure> BASE_MEASURES = concatenate(PHONETIC_COMPARATORS);
+//    public static final List<StringMeasure> BASE_MEASURES = concatenate(TRUE_METRICS, NON_METRIC_MEASURES, PHONETIC_COMPARATORS);
 
     public static String stringRepresentationOf(List<Integer> fields, Class<? extends LXP> record, List<String> labels) {
         StringBuilder sb = new StringBuilder();
