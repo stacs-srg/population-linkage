1ac0a4fb33735f9a88a4803b6d732892099f728e
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2022-08-05 16:52:58 +0100

    Updated licence header.

--- src/main/java/uk/ac/standrews/cs/population_linkage/endToEnd/builders/BirthSiblingBundleThenBirthDeathBuilder.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/endToEnd/builders/BirthSiblingBundleThenBirthDeathBuilder.java
@@ -1,6 +1,18 @@
 /*
  * Copyright 2022 Systems Research Group, University of St Andrews:
  * <https://github.com/stacs-srg>
+ *
+ * This file is part of the module population-linkage.
+ *
+ * population-linkage is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * population-linkage is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with population-linkage. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package uk.ac.standrews.cs.population_linkage.endToEnd.builders;
 
