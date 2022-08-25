213abab96a019a0401b2e3ca22c532ee73d8ef29
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2022-08-25 13:33:10 +0100

    Updated linkageAccuracy as a precursor to adding all accuracy measures (and getting rid of GT from recipes).

---
+++ src/main/java/uk/ac/standrews/cs/population_linkage/linkageAccuracy/AbstractAccuracy.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2022 Systems Research Group, University of St Andrews:
+ * <https://github.com/stacs-srg>
+ *
+ * This file is part of the module population-linkage.
+ *
+ * population-linkage is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * population-linkage is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with population-linkage. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.population_linkage.linkageAccuracy;
+
+import org.neo4j.driver.Result;
+import uk.ac.standrews.cs.neoStorr.util.NeoDbCypherBridge;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import static uk.ac.standrews.cs.utilities.ClassificationMetrics.*;
+import static uk.ac.standrews.cs.utilities.ClassificationMetrics.specificity;
+
+public abstract class AbstractAccuracy {
+
+    protected final NeoDbCypherBridge bridge;
+
+    protected static final String ALL_BIRTHS = "MATCH (r:Birth) return count(r)"; // bad name r but saves writing another method
+    protected static final String ALL_DEATHS = "MATCH (r:Death) return count(r)"; // bad name r but saves writing another method
+    protected static final String ALL_MARRIAGES = "MATCH (r:Marriage) return count(r)"; // bad name r but saves writing another method
+
+    public AbstractAccuracy(NeoDbCypherBridge bridge) {
+        this.bridge = bridge;
+    }
+
+    protected NeoDbCypherBridge getBridge() {
+        return bridge;
+    }
+
+    protected void report(long fpc,long tpc,long fnc,long all_pair_count) {
+
+        System.out.println( this.getClass().getSimpleName() );
+
+        long tnc = all_pair_count - fpc - tpc - fnc;
+
+        double prec = precision(tpc, fpc);
+        double rec = recall(tpc, fnc);
+        double f = F1(tpc, fpc, fnc);
+        double spec = specificity( tnc, fpc );
+
+        NumberFormat format = new DecimalFormat("0.00");
+
+        System.out.println( "False positive count\t" + fpc );
+        System.out.println("True positive count\t" + tpc  );
+        System.out.println("False negative count\t" + fnc  );
+        System.out.println("True negative count\t" + tnc  );
+        System.out.println();
+        System.out.println("Precision\t" + format.format( prec ) );
+        System.out.println("Recall\t" + format.format( rec ) );
+        System.out.println("F1\t" + format.format( f ) );
+        System.out.println("Specificity\t" + format.format( spec ) );
+        System.out.println();
+        System.out.println("----");
+    }
+
+    protected long doQuery(String query_string) {
+        Map<String, Object> parameters = new HashMap<>();
+        Result result = bridge.getNewSession().run(query_string, parameters);
+        long value = result.list(r -> r.get("count(r)").asInt()).get(0);
+        return value;
+    }
+
+}
--- src/main/java/uk/ac/standrews/cs/population_linkage/linkageAccuracy/BirthDeathSiblingAccuracy.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/linkageAccuracy/BirthDeathSiblingAccuracy.java
@@ -1,77 +1,43 @@
+/*
+ * Copyright 2022 Systems Research Group, University of St Andrews:
+ * <https://github.com/stacs-srg>
+ *
+ * This file is part of the module population-linkage.
+ *
+ * population-linkage is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * population-linkage is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with population-linkage. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.standrews.cs.population_linkage.linkageAccuracy;
 
-import org.neo4j.driver.Result;
 import uk.ac.standrews.cs.neoStorr.util.NeoDbCypherBridge;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.HashMap;
-import java.util.Map;
-
-import static uk.ac.standrews.cs.utilities.ClassificationMetrics.*;
-
-public class BirthDeathSiblingAccuracy {
+public class BirthDeathSiblingAccuracy extends AbstractAccuracy {
 
     private static final String BIRTH_DEATH_SIBLING_TPC = "MATCH (b:Birth)-[r:SIBLING]-(d:Death) WHERE (b)-[:GROUND_TRUTH_BIRTH_DEATH_SIBLING]-(d) return count(r)";
     private static final String BIRTH_DEATH_SIBLING_FPC = "MATCH (b:Birth)-[r:SIBLING]-(d:Death) WHERE NOT (b)-[:GROUND_TRUTH_BIRTH_DEATH_SIBLING]-(d) return count(r)";
     private static final String BIRTH_DEATH_SIBLING_FNC = "MATCH (b:Birth)-[r:GROUND_TRUTH_BIRTH_DEATH_SIBLING]-(d:Death) WHERE NOT (b)-[:SIBLING]-(d) return count(r)";
-    private static final String ALL_BIRTHS = "MATCH (r:Birth) return count(r)"; // bad name r but saves writing another method
-    private static final String ALL_DEATHS = "MATCH (r:Death) return count(r)"; // bad name r but saves writing another method
-
-
-    public NeoDbCypherBridge getBridge() {
-        return bridge;
-    }
-
-    private final NeoDbCypherBridge bridge;
 
     public BirthDeathSiblingAccuracy(NeoDbCypherBridge bridge) {
-        this.bridge = bridge;
+        super(bridge);
     }
 
     private void doqueries() {
-
         long fpc = doQuery(BIRTH_DEATH_SIBLING_FPC);
         long tpc = doQuery(BIRTH_DEATH_SIBLING_TPC);
         long fnc = doQuery(BIRTH_DEATH_SIBLING_FNC);
-        report(fpc,tpc,fnc);
-    }
-
-    private void report(long fpc,long tpc,long fnc) {
-
-        System.out.println( this.getClass().getSimpleName() );
-
 
         long birth_count = doQuery( ALL_BIRTHS );
         long death_count = doQuery( ALL_DEATHS );
+        long all_pair_count = birth_count*death_count;
 
-        long tnc = ( birth_count * death_count ) - fpc - tpc - fnc;
-
-        double prec = precision(tpc, fpc);
-        double rec = recall(tpc, fnc);
-        double f = F1(tpc, fpc, fnc);
-        double spec = specificity( tnc, fpc );
-
-        NumberFormat format = new DecimalFormat("0.00");
-
-        System.out.println( "False positive count\t" + fpc );
-        System.out.println("True positive count\t" + tpc  );
-        System.out.println("False negative count\t" + fnc  );
-        System.out.println("True negative count\t" + tnc  );
-        System.out.println();
-        System.out.println("Precision\t" + format.format( prec ) );
-        System.out.println("Recall\t" + format.format( rec ) );
-        System.out.println("F1\t" + format.format( f ) );
-        System.out.println("Specificity\t" + format.format( spec ) );
-        System.out.println();
-        System.out.println("----");
-    }
-
-    private long doQuery(String query_string) {
-        Map<String, Object> parameters = new HashMap<>();
-        Result result = bridge.getNewSession().run(query_string, parameters);
-        long value = result.list(r -> r.get("count(r)").asInt()).get(0);
-        return value;
+        report(fpc,tpc,fnc,all_pair_count);
     }
 
     public static void main(String[] args) {
@@ -80,10 +46,4 @@ public class BirthDeathSiblingAccuracy {
             acc.doqueries();
         }
     }
-
-
-
-
-
-
 }
