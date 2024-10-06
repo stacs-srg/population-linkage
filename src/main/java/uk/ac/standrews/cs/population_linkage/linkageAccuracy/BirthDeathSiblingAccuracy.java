d67b3fabb04657dfe449f917dab472d683a33de5
Aleksandr Isaev <ai52@st-andrews.ac.uk>
2024-10-06 18:03:13 +0100

    accuracy fixes

--- src/main/java/uk/ac/standrews/cs/population_linkage/linkageAccuracy/BirthDeathSiblingAccuracy.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/linkageAccuracy/BirthDeathSiblingAccuracy.java
@@ -20,12 +20,13 @@ import uk.ac.standrews.cs.neoStorr.util.NeoDbCypherBridge;
 
 public class BirthDeathSiblingAccuracy extends AbstractAccuracy {
 
-    private static final String BIRTH_DEATH_SIBLING_TPC = "MATCH (b:Birth)-[r:SIBLING {actors: \"Child-Deceased\"}]-(d:Death) WHERE (b)-[:GT_SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
-    private static final String BIRTH_DEATH_SIBLING_FPC = "MATCH (b:Birth)-[r:SIBLING {actors: \"Child-Deceased\"}]-(d:Death) WHERE NOT (b)-[:GT_SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
-    private static final String BIRTH_DEATH_SIBLING_FNC = "MATCH (b:Birth)-[r:GT_SIBLING {actors: \"Child-Deceased\"}]-(d:Death) WHERE NOT (b)-[:SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
+    private static final String BIRTH_DEATH_SIBLING_TPC = "MATCH (b:Birth)-[r:SIBLING {actors: \"Deceased-Child\"}]-(d:Death) WHERE (b)-[:GT_SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
+    private static final String BIRTH_DEATH_SIBLING_FPC = "MATCH (b:Birth)-[r:SIBLING {actors: \"Deceased-Child\"}]-(d:Death) WHERE NOT (b)-[:GT_SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
+    private static final String BIRTH_DEATH_SIBLING_FNC = "MATCH (b:Birth)-[r:GT_SIBLING {actors: \"Deceased-Child\"}]-(d:Death) WHERE NOT (b)-[:SIBLING {actors: \"Child-Deceased\"}]-(d) return count(r)";
 
     public BirthDeathSiblingAccuracy(NeoDbCypherBridge bridge) {
         super(bridge);
+        doqueries();
     }
 
     private void doqueries() {
