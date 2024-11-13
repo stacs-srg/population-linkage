e7ce0e09a15f963e314a3bbd0a33bf9b73cf0f68
Aleksandr Isaev <ai52@st-andrews.ac.uk>
2024-11-13 21:17:42 +0000

    Update ThresholdTrianglesAnalysisParallel.java

--- src/main/java/uk/ac/standrews/cs/population_linkage/aleks/ThresholdTrianglesAnalysisParallel.java
+++ src/main/java/uk/ac/standrews/cs/population_linkage/aleks/ThresholdTrianglesAnalysisParallel.java
@@ -74,10 +74,10 @@ public class ThresholdTrianglesAnalysisParallel {
 
     public static void main(String[] args) throws InterruptedException {
         NeoDbCypherBridge bridge = new NeoDbCypherBridge();
-        final int MAX_FIELD = 4;
-        final int MIN_FIELD = 2; //1 below target
-        final double MAX_THRESHOLD = 2.01; //0.01 above target
-        final double MIN_THRESHOLD = 1.88;
+        final int MAX_FIELD = 2;
+        final int MIN_FIELD = 1; //1 below target
+        final double MAX_THRESHOLD = 1.01; //0.01 above target
+        final double MIN_THRESHOLD = 0.23;
 
 
         ExecutorService executorService = Executors.newFixedThreadPool(MAX_FIELD - MIN_FIELD);
