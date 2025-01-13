15c7b9be41b18be1fbd7601ef5d966a35810c83d
Aleksandr Isaev <ai52@st-andrews.ac.uk>
2025-01-13 13:37:23 +0000

    Create runall.sh

---
+++ src/main/scripts/endtoend/runall.sh
@@ -0,0 +1,3 @@
+. "$(dirname $0)"/../../java/uk/ac/standrews/cs/population_linkage/aleks/resolvers/run_resolvers.sh
+. "$(dirname $0)"/dolinkage_template.sh EVERYTHING
+. "$(dirname $0)"/../../java/uk/ac/standrews/cs/population_linkage/aleks/analysers/run_threshold_analysis.sh
